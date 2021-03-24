5c89f77ae07a2562f126e99a29ba776a525c76db
RiturajBhattacharjee <77571186+RiturajBhattacharjee@users.noreply.github.com>
2021-03-24 17:36:27 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,30 @@
+class Form{
+    constructor(){
+        this.title=createElement("h1")
+       this.input=createInput("name")
+        this.button = createButton("Start")
+      this.greeting=createElement("h2")
+    }
+
+    display(){
+        
+       this.title.html("asphalt5")
+       this.title.position(width/2-50,0)
+        
+       this.input.position(width/2-70,height/2-80)
+      
+       this.button.position(width/2+30,height/2)
+       this.button.mousePressed(()=> {
+        this.input.hide()
+        this. button.hide()
+           player.name =  this.input.value()
+            playerCount++ 
+            player.index=playerCount
+            player.updateCount(playerCount)
+            player.updateInfo()
+           
+            this.greeting.html("Hello " + player.name)
+            this. greeting.position(width/2-70,height/4)
+        })
+    }
+}
